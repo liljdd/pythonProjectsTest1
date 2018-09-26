efd1d86f7f7aa4a05c2de66f71cffa2b7d13dd69
lilj <lilj@mchina.cn>
2018-09-26 10:05:46 +0800

    dateStructure

--- DataStructure.py
+++ DataStructure.py
@@ -39,7 +39,25 @@ print(newfruit)
 print('-------------------------------------')
 knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 for i in knights.items():
-    print(i)    # ('gallahad', 'the pure')
+    print(i)  # ('gallahad', 'the pure')
 
 for key, value in knights.items():
     print(key, '=', value)  # gallahad = the pure
+
+print("++++++++++++++++++++++++++++++++++++++++++")
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}?  It is {1}.'.format(q, a))
+
+print("++++++++++++++++++++反向遍历一个序列++++++++++++++++++++++")
+for i in reversed(range(1, 10, 2)):
+    print(i, end=",")
+
+print("++++++++++++++++++++使用 sorted() 函数返回一个已排序的序列++++++++++++++++++++++")
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+for i in sorted(basket):
+    print(i, end=",")
+print()  # 换行
+for i in sorted(set(basket)):
+    print(i, end=",")
